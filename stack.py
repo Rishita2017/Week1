eeb283fcbab3e222ca1a9588cb1c5c5dd5101d90
Rishita2017 <96405597+Rishita2017@users.noreply.github.com>
2022-01-07 11:36:56 +0530

    Update and rename Stack.py to stack.py

--- stack.py
+++ stack.py
@@ -1,15 +1,18 @@
 import boto3
-client_cloud=boto3.client(‘cloudformation’)
+
 class Stack:
-    def __init__(self, stack_name, template_body,  source_bucket , data_bucket):
+    def __init__(self, stack_name, template_body, region, source_bucket, data_bucket):
         self.stack_name = stack_name
         self.template_body = template_body
-        self.source_bucket= source_bucket
+        self.client_cloudformation = boto3.client('cloudformation', region_name=region)
+        self.client_s3 = boto3.client('s3', region_name=region)
+        self.source_bucket = source_bucket
         self.data_bucket = data_bucket
 
-
     def create_stack(self):
-        response = self.client_cloud.create_stack(
+        print(self.source_bucket)
+        print(self.data_bucket)
+        self.client_cloudformation.create_stack(
             StackName=self.stack_name,
             TemplateBody=self.template_body,
             Capabilities=['CAPABILITY_IAM'],
@@ -22,10 +25,12 @@ class Stack:
                     'ParameterKey': "DataBucket",
                     'ParameterValue': self.data_bucket
                 }
+
             ]
         )
+
     def update_stack(self):
-        response1 = self.client_cloud.update_stack(
+       self.client_cloudformation.update_stack(
             StackName=self.stack_name,
             TemplateBody=self.template_body,
             Capabilities=['CAPABILITY_IAM'],
@@ -37,9 +42,10 @@ class Stack:
                 {
                     'ParameterKey': "DataBucket",
                     'ParameterValue': self.data_bucket
-                }
+                }])
+
     def status_check(self):
-        stack_status== self.client_cloud.describe_stacks(StackName=self.stack_name)
+        stack_status = self.client_cloudformation.describe_stacks(StackName=self.stack_name)
         status = stack_status['Stacks'][0]['StackStatus']
         return status
 
