2eeffdec6a06a7d75d947eab6f6dde66525e794f
Rishita2017 <96405597+Rishita2017@users.noreply.github.com>
2022-01-31 14:24:39 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,19 @@
 import json
 
 def my_handler(event, context):
-    print("Hello world")
-    return {
+     s3=boto3.client("s3")
+     if event:
+        print("Event : ",event)
+        fil_obj=event["Record"][0]
+        filename=str(fil_obj['s3']['object']['key'])
+        print("ilename:",filename)
+        fileobj=s3.get_object(Bucket=Sourcebucket,Key=filename)
+        print("fileobj",fileobj)
+        filecontent=fileobj["Body"].read().decode('utf-8')
+        print(filecontent)
+       
+   
+   return {
         'statusCode': 200,
         'body': json.dumps('Hello for lambda')
     }
