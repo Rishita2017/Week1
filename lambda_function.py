23a600b776b59edf6341a4ebf3bfaa0d6e84305d
RISHITA JAIN <jainrishita2000@gmail.com>
2022-02-07 11:29:52 +0530

    lines changed

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,11 @@
+import boto3
 import json
+client_s3 = boto3.client('s3')
 
 def my_handler(event, context):
-     s3=boto3.client("s3")
-     if event:
-        print("Event : ",event)
-        fil_obj=event["Record"][0]
-        filename=str(fil_obj['s3']['object']['key'])
-        print("ilename:",filename)
-        fileobj=s3.get_object(Bucket=Sourcebucket,Key=filename)
-        print("fileobj",fileobj)
-        filecontent=fileobj["Body"].read().decode('utf-8')
-        print(filecontent)
-       
-   
-   return {
-        'statusCode': 200,
-        'body': json.dumps('Hello for lambda')
-    }
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        response = client_s3.get_object(Bucket=bucket, Key=key)
+    print("Author : " + response['Metadata']['author'])
+    print("Description : " + response['Metadata']['description'])
